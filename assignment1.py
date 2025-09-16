3cad792cffe1c976d6963e04fd68e33aab5d0e5f
naten8564-pixel <naten8564@gmail.com>
2025-09-16 16:30:05 +0300

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
 name="Noel"
 age=18
-favourite_foods="chips","chocolate","pizza"
+favourite_foods="chips","chocolate","ice cream"
 print(f"My name is {name}.")
 print(f"I am {age} years old.")
 print(f"My favourite foods are {favourite_foods}.")
